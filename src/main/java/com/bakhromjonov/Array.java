76957276cf8be5f61346f013bb78a21250922312
Khurshidbek Bakhromjonov <khurshidbekjava@gmail.com>
2023-03-18 20:50:21 +0500

    removeAt()

--- src/main/java/com/bakhromjonov/Array.java
+++ src/main/java/com/bakhromjonov/Array.java
@@ -23,6 +23,16 @@ public class Array {
         count++;
     }
 
+    public void removeAt(int index) {
+        if (index < 0 || index >= count)
+            throw new IllegalArgumentException();
+
+        for (int i = index; i < count; i++)
+            items[i] = items[i + 1];
+
+        count--;
+    }
+
     public void print() {
         for (int i = 0; i < count; i++)
             System.out.println(items[i]);
