3517ea4df53623fa1b68908defc1de188e4a90cd
Khurshidbek Bakhromjonov <khurshidbekjava@gmail.com>
2023-03-21 11:02:03 +0500

    LinkedList addFirst(), indexOf(), contains()

--- src/main/java/com/bakhromjonov/Main.java
+++ src/main/java/com/bakhromjonov/Main.java
@@ -3,10 +3,5 @@ package com.bakhromjonov;
 public class Main {
 
     public static void main(String[] args) {
-        LinkedList list = new LinkedList();
-        list.addLast(10);
-        list.addLast(20);
-        list.addLast(30);
-        System.out.println(list.contains(20));
     }
 }
